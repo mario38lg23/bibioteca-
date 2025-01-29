686f2ebb34f355834402501c22eeec395fc45423
mario38lg23 <mariolopezgas@gmail.com>
2025-01-29 21:02:14 +0100

    commit final 2

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -103,62 +103,7 @@ public class Biblioteca {
             System.out.println("No hay libros prestados");
             
         }
-    }/* 
-    public void buscarLibro(int opcion){
-        boolean libroEncontrado= false;
-        Scanner sc = new Scanner(System.in);
-        
-        switch (opcion) {
-            case 1:
-            System.out.println("Dime el titulo que deseas buscar ");
-            String titulo = sc.nextLine();
-                for(int i= 0; i<numLibros;i++){
-                    if (titulo.equals(libros[i].getTitulo())) {
-                        System.out.println("El autor de este libro es " + libros[i].getAutor());
-                        System.out.println("La categoria de este libro es " + libros[i].getCategoria());
-                        libroEncontrado= true;
-                    }
-                    
-                }
-                if (!libroEncontrado) {
-                    System.out.println("No se encontró un libro con ese título.");
-                }
-                break;
-                case 2:
-                System.out.println("Dime el autor que deseas buscar");
-                String autor = sc.nextLine();
-                for (int i = 0; i<numLibros;i++){
-                    if (autor.equals(libros[i].getAutor())) {
-                        System.out.println("El titulo de este libro es " + libros[i].getTitulo());
-                        System.out.println("La categoria de este libro es " + libros[i].getCategoria());
-                        libroEncontrado= true;
-                    }
-                
-                }
-                if (!libroEncontrado) {
-                    System.out.println("No se encontró un libro con ese autor");
-                }
-                break;
-                case 3:
-                System.out.println("Dime la cateoria que deseas");
-                String categoria = sc.nextLine();
-                for(int i=0; i<numLibros;i++){
-                    if (categoria.equals(libros[i].getCategoria())) {
-                        System.out.println("El titulo de este libro es " + libros[i].getTitulo());
-                        System.out.println("El autor de este libro es " + libros[i].getAutor());
-                        libroEncontrado= true;
-                    }
-                }
-                if (!libroEncontrado) {
-                    System.out.println("No se encontró un libro con esa categoria.");
-                }
-                break;
-        
-            default:
-            System.out.println("Selecciona una opcion correcta ");
-                break;
-        }
-    }*/
+    }
         public void buscarPorTitulo(String titulo){
             boolean encontrado = false;
             for (int i = 0; i < numLibros; i++) {
